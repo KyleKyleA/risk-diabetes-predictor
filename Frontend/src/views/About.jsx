98b7b1bb1a10d393df152d145512c0c42e86bcee
Kyle Angeles <kyle.angeles@dcmail.ca>
2026-08-10 23:58:24 -0400

    just fixing up the background but im mostly done for today did the login form, styling, and questionairre today good grinding

--- Frontend/src/views/About.jsx
+++ Frontend/src/views/About.jsx
@@ -8,17 +8,17 @@ function About() {
 
     return (
         
-        <div className="max-w-5xl mx-auto px-6 pt-24 pb-16">
-
-            <h2 className="text-white-400 text-center">About</h2>
+        <section className="max-w-5xl mx-auto px-6 pt-24 pb-16">
+        <div className="w-full p-8 rounded-2xl bg-grey border border-[var(--border)] shadow-2xl shadow-black/80">
+               <h2 className="text-white-400 text-center">About</h2>
             <p className="text-white-400 text-center mt-4">
                 Risk Diabetes Predictor is essentially a web application that combines AI and Machine learning to predict the risks of diabetes based on the user's information. 
                 Web application is built using React, TailwindCSS, and Vite. The AI model is undetermined at this moment but it will be trained using a current dataset of diabetes patients and their information.
                 This application is a personal project of mine that I'm currently building to learn more about AI and Machine learning. The idea behind this application is to help me focus on real world problems 
                 and how to solve them using Analytics and Machine learning. 
             </p>
-
         </div>
+        </section>
     )
 }
 
