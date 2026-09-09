64b11b431e1087916c735ae09dff7d409100885e
Kyle Angeles <kyle.angeles@dcmail.ca>
2026-09-09 15:59:18 -0400

    setting up the component structure that we are going be using from framer for our dashboard

---
+++ Frontend/src/Components/RiskCalc.jsx
@@ -0,0 +1,21 @@
+// Author: Kyle Angeles
+// File-Name: RiskCalc.jsx
+// Description: This component handles the risk calculating derived from the Ml_pipeline folder 
+// once we have trained that it would be handled into here calculating the risk of the user based off their health style and other factors.
+
+
+function RiskCalc() {
+
+
+    return (
+
+
+        <>
+        
+        
+        
+        </>
+    )
+}
+
+export default RiskCalc;
---
+++ Frontend/src/Components/StatChart.jsx
@@ -0,0 +1,17 @@
+// Author: Kyle Angeles
+// File-Name: StatChart.jsx
+// Description: This component handles the frontend for displaying certain charts 
+
+function StatChart() {
+
+
+    return (
+
+        <>
+        
+        
+        </>
+    )
+}
+
+export default StatChart;
