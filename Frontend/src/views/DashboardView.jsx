d4afdda0a436b1543b946d8b219b031d086a6d61
Kyle Angeles <kyle.angeles@dcmail.ca>
2026-09-19 22:02:39 -0400

    login finally works with the submit just to fix the dashboard

--- Frontend/src/views/DashboardView.jsx
+++ Frontend/src/views/DashboardView.jsx
@@ -1,10 +1,11 @@
 import DiabetesDashboard from "../Components/DashBoardComponent.jsx";
 import { useLocation } from "react-router-dom";
-
+import { riskResult} from "../Components/DashBoardComponent.jsx"
 
 function DashboardView() {
 
-    const { riskResult } = useRisk();
+    const location = useLocation();
+    const { userData, prediction } = location.state ?? {};
 
     if (!riskResult) {
         return <p>No Data yet - complete the questionnaire first</p>
